d184f1e7753b3a5e4d0dd9e56319f1ea7e4d13b2
MICHEAL TURNER <b7040899@hallam.shu.ac.uk>
2018-03-28 12:34:28 +0100

    test

--- CarParkSimulator/FullSign.cs
+++ CarParkSimulator/FullSign.cs
@@ -7,5 +7,20 @@ namespace CarParkSimulator
 {
     class FullSign
     {
+        private bool lit;
+
+        public bool isLit(bool lit)
+        {
+            if (currentSpaces == 0)
+            {
+                lit = true;
+            }
+            else
+                lit = false;
+        }
+        public void SetLit(bool lit)
+        {
+            this.lit = lit;
+        }
     }
 }
